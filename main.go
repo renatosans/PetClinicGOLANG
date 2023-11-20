39e7672f5c7d197e3e161a9279a6ff5e43458840
renatosans <renato@datacopy.com.br>
2023-11-20 15:31:07 -0300

    Ajustes

--- main.go
+++ main.go
@@ -27,8 +27,8 @@ func main() {
 	pets = append(pets, Pet {Id:1, Name: "Bethoven", Breed: "Saint Bernard", Age: 7, Owner: nil})
 	pets = append(pets, Pet {Id:2, Name: "Molly", Breed: "Golden Retriever", Age: 4, Owner: nil})
 	pets = append(pets, Pet {Id:3, Name: "Yoshi", Breed: "Shiba Inu", Age: 2, Owner: nil})
-	pets = append(pets, Pet {Id:4, Name: "Thor", Breed: "Beagle", Age: 9, Owner: nil})
-	pets = append(pets, Pet {Id:4, Name: "Hulk", Breed: "Pit Bull", Age: 5, Owner: nil})
+	pets = append(pets, Pet {Id:4, Name: "Luigi", Breed: "Beagle", Age: 9, Owner: nil})
+	pets = append(pets, Pet {Id:5, Name: "Hulk", Breed: "Pit Bull", Age: 5, Owner: nil})
 
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
