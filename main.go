cd34c520ea4450e6ec631058ec76973f2bf6c2a1
renatosans <renato@datacopy.com.br>
2023-11-20 11:03:33 -0300

    Ajustes

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    fmt.Println("Hello, World!")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hell yeah",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
