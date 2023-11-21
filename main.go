664a02c80a784db91eaaa4e93d2677c91ffc22eb
renatosans <renato@datacopy.com.br>
2023-11-21 17:40:23 -0300

    Ajustes

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strconv"
 	"net/http"
 	"github.com/gin-gonic/gin"
 )
@@ -38,6 +39,25 @@ func postPet(c *gin.Context) {
 	c.JSON(http.StatusCreated, gin.H{"message": "Pet created successfully", "pet": newPet})
 }
 
+func patchPet(c *gin.Context) {
+	// Get the ID from the URL parameters
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	// Bind JSON body to the Pet struct for updates
+	var updatedPet Pet
+	if err := c.ShouldBindJSON(&updatedPet); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Pet " + strconv.Itoa(id) + " updated successfully", "pet": updatedPet})
+}
+
 func main() {
 	pets = append(pets, Pet {Id:1, Name: "Bethoven", Breed: "Saint Bernard", Age: 7, Owner: nil})
 	pets = append(pets, Pet {Id:2, Name: "Molly", Breed: "Golden Retriever", Age: 4, Owner: nil})
@@ -55,7 +75,7 @@ func main() {
     rGroup := r.Group("/api");
 	rGroup.GET("/pets", getPets);
 	rGroup.POST("/pets", postPet);
-	// rGroup.PATCH("/pets/{pet_id}", patchPet);
+	rGroup.PATCH("/pets/:id", patchPet);
 	// rGroup.DELETE("/pets/{pet_id}", deletePet);
 
 	r.Run() // listen and serve on 0.0.0.0:8080
