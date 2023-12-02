865e0b54d49c7f2910ce9fc6cb38799f1d6c7887
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2023-12-02 15:03:24 -0300

    Adicionado CORS a API

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"petClinicAPI/prisma/db"
 	"strconv"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	// "github.com/joho/godotenv"
 )
@@ -116,18 +117,19 @@ func deletePet(c *gin.Context) {
 func main() {
 	// godotenv.Load(".env")
 
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Gin gonic API",
 		})
 	})
 
-	rGroup := r.Group("/api")
+	rGroup := router.Group("/api")
 	rGroup.GET("/pets", getPets)
 	rGroup.POST("/pets", postPet)
 	rGroup.PATCH("/pets/:id", patchPet)
 	rGroup.DELETE("/pets/:id", deletePet)
 
-	r.Run(":3000") // listen and serve on 0.0.0.0:3000
+	router.Use(cors.Default())
+	router.Run(":3000") // listen and serve on 0.0.0.0:3000
 }
